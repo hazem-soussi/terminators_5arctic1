b667e48defd1447418740a9d3ffafefe189dfc45
SiwarHassine <siwar.hassine@esprit.tn>
2022-10-25 03:43:45 +0100

    docker_compose20

--- src/main/java/com/esprit/examen/TpAchatProjectApplication.java
+++ src/main/java/com/esprit/examen/TpAchatProjectApplication.java
@@ -2,10 +2,11 @@ package com.esprit.examen;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+@EnableAutoConfiguration
 @EnableScheduling
 @SpringBootApplication
 public class TpAchatProjectApplication {
